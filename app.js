074dd1bfb2cf0aaf85b197ea0e61621a93adc492
Emanuel Amerio <83380305+emarose@users.noreply.github.com>
2021-11-16 16:51:16 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+const methodOverride = require("method-override");
+const cors = require("cors");
+const express = require("express");
+
+//
+const app = express();
+const log = console.log;
+
+let port = process.env.PORT || 3000;
+
+let users = ["bart", "lisa", "homero", "marge"]; 
+
+app.use(cors());
+app.use(methodOverride());
+
+app.get("/users", (req, res)=>{
+   res.send(users);
+});
+
+app.post("/user/create/:nombre", (req, res)=>{
+   users.push(req.params.nombre);
+   res.send("usuario creado");
+});
+
+app.put('/user/:viejo/:nuevo', (req,res)=>{
+  users = users.filter(user => user !== req.params.viejo);
+  users.push(req.params.nuevo);
+  res.send('Usuario actualizado');
+})
+
+app.listen(port, ()=>{
+    log("start server");
+});
+
+app.delete("/user/:nombre", (req,res)=>{
+  users = users.filter(user => users !== req.params.nombre);
+  res.send('usuario borrado');
+});
